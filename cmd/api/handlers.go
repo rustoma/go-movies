f2293cb6278e8463a1a83e79ac2b030a28eb3deb
rustoma <rutoma00@gmail.com>
2023-02-23 06:33:43 +0100

    feat: add fake movie endpoint

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,10 +1,73 @@
 package main
 
 import (
+	"backend/internal/models"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello world from %s", app.Domain)
+	var payload = struct {
+		Status  string `json:"status"`
+		Message string `json:"message"`
+		Version string `json:"version"`
+	}{
+		Status:  "active",
+		Message: "Go movies up and running",
+		Version: "1.0.0",
+	}
+
+	out, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
+}
+
+func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
+
+	var movies []models.Movie
+
+	releaseDate, _ := time.Parse("2006-01-02", "1986-03-07")
+
+	highlander := models.Movie{
+		ID:          1,
+		Title:       "Highlander",
+		ReleaseDate: releaseDate,
+		MPAARating:  "R",
+		RunTime:     116,
+		Description: "Nice movie",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+
+	releaseDate, _ = time.Parse("2006-01-02", "1981-06-12")
+
+	raiders := models.Movie{
+		ID:          2,
+		Title:       "Raiders",
+		ReleaseDate: releaseDate,
+		MPAARating:  "PG-13",
+		RunTime:     115,
+		Description: "Nice Raiders movie",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+
+	movies = append(movies, highlander)
+	movies = append(movies, raiders)
+
+	out, err := json.Marshal(movies)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
 }
