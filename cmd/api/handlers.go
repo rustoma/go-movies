60da58d97a39b99683b5414a373b3c7f7a0a3bef
rustoma <rutoma00@gmail.com>
2023-03-05 06:54:21 +0100

    feat: add GetMovie and MovieForEdit handlers

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"backend/internal/models"
 	"errors"
+	"github.com/go-chi/chi/v5"
 	"github.com/golang-jwt/jwt/v4"
 	"log"
 	"net/http"
@@ -136,5 +138,52 @@ func (app *application) logout(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) MovieCatalog(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) GetMovie(w http.ResponseWriter, r *http.Request) {
+
+	id := chi.URLParam(r, "id")
+
+	movieID, err := strconv.Atoi(id)
+	if err != nil {
+		_ = app.errorJSON(w, err)
+		return
+	}
+
+	movie, err := app.DB.OneMovie(movieID)
+	if err != nil {
+		_ = app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	_ = app.writeJSON(w, http.StatusOK, movie)
+}
+
+func (app *application) MovieForEdit(w http.ResponseWriter, r *http.Request) {
+
+	id := chi.URLParam(r, "id")
+
+	movieID, err := strconv.Atoi(id)
+	if err != nil {
+		_ = app.errorJSON(w, err)
+		return
+	}
+
+	movie, genres, err := app.DB.OneMovieForEdit(movieID)
+	if err != nil {
+		_ = app.errorJSON(w, err)
+		return
+	}
+
+	var payload = struct {
+		Movie  *models.Movie   `json:"movie"`
+		Genres []*models.Genre `json:"genres"`
+	}{
+		movie,
+		genres,
+	}
+
+	_ = app.writeJSON(w, http.StatusOK, payload)
 
 }
